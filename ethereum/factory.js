6d0398ee18d1216317963b135b8ecfeb10948e32
Wesam Mustafa <wesam.mustafa100@gmail.com>
2019-06-18 17:38:22 +0200

    Initialize front end using next js

---
+++ ethereum/factory.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+import CampaignFactory from './build/campaignFactory.json';
+
+const  instance = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  '0xa6D1AA92eE789DA9D15DbEC65636Cdafd6574b81'
+);
+
+export default instance;
