8fccdf8f549eabfff6ebe9b89dff86e5136f975d
Wesam Mustafa <wesam.mustafa100@gmail.com>
2019-06-20 00:57:18 +0200

    initialize create request

---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RequestNew extends Component {
+  render() {
+    return (
+      <h3>
+        Create Request
+      </h3>
+    );
+  }
+}
+
+export default RequestNew;
