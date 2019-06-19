60c1a0c52903bb7297b6f1b71bb7c6930bafc762
Wesam Mustafa <wesam.mustafa100@gmail.com>
2019-06-20 00:21:07 +0200

    Finish contribute form component

---
+++ pages/campaigns/show.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Card, Grid } from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
+
+class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const campaign = Campaign(props.query.address);
+    const summary = await campaign.methods.getSummary().call();
+    return {
+      address: props.query.address,
+      minimumContributions: summary[0].toString(),
+      balance: web3.utils.fromWei(summary[1].toString(), 'ether'),
+      requestsCount: summary[2].toString(),
+      approversCount: summary[3].toString(),
+      manager: summary[4]
+    };
+  }
+
+  renderCardS() {
+    const {
+      minimumContributions,
+      balance,
+      requestsCount,
+      approversCount,
+      manager
+    } = this.props;
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of Manager',
+        description: 'The manager created this campaign and can create requests to withdraw money',
+        style: { overflowWrap: 'break-word' }
+      },
+      {
+        header: minimumContributions,
+        meta: 'Minimum Contribution (Wei)',
+        description: 'You must contribute at least this much wei in order to become an approver'
+      },
+      {
+        header: requestsCount,
+        meta: 'Number of Requests',
+        description: 'A request tries to withdraw money from the contract. Requests must be approved by approvers '
+      },
+      {
+        header: approversCount,
+        meta: 'Number of Approvers',
+        description: 'Number of people who already donated to this campaign'
+      },
+      {
+        header: balance,
+        meta: 'Campaign Balance (ether)',
+        description: 'The balance is how much money this campaign has left to spend.'
+      }
+    ];
+
+    return <Card.Group items={items} />;
+  }
+  render() {
+    return (
+      <Layout>
+        <h3>Campaign Show</h3>
+        <Grid>
+          <Grid.Column width={10}>
+            {this.renderCardS()}
+          </Grid.Column>
+
+          <Grid.Column width={6}>
+            <ContributeForm address={this.props.address}/>
+          </Grid.Column>
+        </Grid>
+      </Layout>
+    );
+  }
+}
+
+export default CampaignShow;
