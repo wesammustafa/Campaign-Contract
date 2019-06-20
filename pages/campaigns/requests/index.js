a986d00738887b315a63e3aa6e8e7a0c4774fe0b
Wesam Mustafa <wesam.mustafa100@gmail.com>
2019-06-20 04:06:08 +0200

    Finish new request component

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
+    const campaign = Campaign(address);
+    const requestsCount = await campaign.methods.getRequestsCount().call();
 
-    return { address };
+    const requests = await Promise.all(
+      Array(requestsCount)
+      .fill()
+      .map((element, index) => {
+          return campaign.methods.requests(index).call();
+      })
+    )
+    console.log(requests);
+    return { address, requests, requestsCount };
   }
   render() {
     return (
